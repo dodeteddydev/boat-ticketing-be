68ee62aad1569da62b6e2c8fa10374f252484063
dodeteddy <dodeteddydev@gmail.com>
2025-03-06 20:39:37 +0800

    add cors middleware

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { errorMiddleware } from "./middlewares/error-middleware";
 import { notFoundMiddleware } from "./middlewares/not-found-middleware";
 import { userRoute } from "./modules/user/user-route";
+import cors from "cors";
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT;
@@ -9,6 +10,8 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 // ROUTE
 app.use(userRoute);
