c354eec0ebf8753463fb2fbecfad4c7bea691667
dodeteddy <dodeteddydev@gmail.com>
2025-04-05 17:43:35 +0800

    chore: update .gitignore and remove tracked files

--- dist/utilities/date-format.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateFormat = void 0;
-const dateFormat = (date) => {
-    return date.toISOString().replace("T", " ").replace("Z", "");
-};
-exports.dateFormat = dateFormat;
--- dist/utilities/validation.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validation = void 0;
-const validation = (schema, data) => schema.parse(data);
-exports.validation = validation;
